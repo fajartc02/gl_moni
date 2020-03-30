ba3211306af673b41488f581a1c3a8f4a009063e
toyota <toyota@toyotas-MacBook-Pro.local>
2020-03-30 16:21:18 +0700

    config users signup and signin

---
+++ models/modelAchievementAv.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const achievementAvSchema = new Schema(
+  {
+    date: { type: Date, required: true },
+    line: { type: String, required: true },
+    machine: { type: String, required: true },
+    avAchievement: { type: Number, required: true },
+    status: { type: String, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
